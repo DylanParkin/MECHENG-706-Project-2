962176677df2ba174d26fd5b0abd6ece56e77425
Johnson Du <johnsondu@wifi-172-23-116-186.uoa-wifi.auckland.ac.nz>
2026-05-06 19:26:08 +1200

    added new state in fsm AVOID

---
+++ main/avoid.cpp
@@ -0,0 +1,75 @@
+#include "avoid.h"
+
+STATE avoiding() {
+  Avoid();
+
+  return NAVIGATING;
+}
+
+void Avoid() {
+  int dir;
+  bool strafe_right = false;
+  constexpr float low_clearance_threshold = 9.0f;
+  constexpr float high_clearance_threshold = 11.0f;
+  // point servo to right and check right ir
+
+  // ultraServo.writeMicroseconds(550);
+  // delay(500);
+
+  float ultra_right = TriggerUltrasonic();
+  float IR_right = get_right_IR();
+
+  float right_sum = ultra_right + IR_right;
+
+  // ultraServo.writeMicroseconds(2500);
+  // delay(500);
+
+  float ultra_left = TriggerUltrasonic();
+  float IR_left = get_left_IR();
+
+  float left_sum = ultra_left + IR_left;
+
+  // ultraServo.write(90);
+  // delay(300);
+
+  if (right_sum > left_sum) {
+    strafe_right = true;
+  }
+
+  dir = (strafe_right) ? -1 : 1;  // dir +ve strafe left
+
+  bool front_clearance_was_low = false;  // low meaning object in frnt of IR
+  bool side_clearance_was_low = false;   // low meaning object in frnt of IR
+
+  // strafe stage
+  while (true) {
+    float front_clearance_IR = (strafe_right) ? get_front_left_IR() : get_front_right_IR();
+
+    // clear object when strafing
+    if (front_clearance_IR <= low_clearance_threshold) {
+      front_clearance_was_low = true;
+    } else if (front_clearance_was_low && front_clearance_IR >= high_clearance_threshold) {
+      stop();
+      break;
+    }
+
+    left_front_motor.writeMicroseconds(1500 - dir * strafe_speed);
+    left_rear_motor.writeMicroseconds(1500 + dir * strafe_speed);
+    right_rear_motor.writeMicroseconds(1500 + dir * strafe_speed);
+    right_front_motor.writeMicroseconds(1500 - dir * strafe_speed);
+  }
+
+  // drive forward stage
+  while (true) {
+    float side_clearance_IR = (strafe_right) ? get_left_IR() : get_right_IR();
+    // clear object when driving forwards
+
+    if (side_clearance_IR <= low_clearance_threshold) {
+      side_clearance_was_low = true;
+    } else if (side_clearance_was_low && side_clearance_IR >= high_clearance_threshold) {
+      stop();
+      return;
+    }
+    forward();
+  }
+}
---
+++ main/avoid.h
@@ -0,0 +1,10 @@
+#ifndef AVOID_H
+#define AVOID_H
+
+#include "base_code.h"
+#include "sensors.h"
+#include "states.h"
+
+STATE avoiding();
+
+#endif  // AVOID_H
