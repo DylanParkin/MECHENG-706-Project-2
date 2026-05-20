fec89e9ef2226da4ac1357f6c7e00cb2f3ff27ec
DylanParkin <dylanparkin153@gmail.com>
2026-05-20 13:12:25 +1200

    Updated object classification criteria for PTs using 4.7K resistors

--- main/navigate.cpp
+++ main/navigate.cpp
@@ -19,12 +19,12 @@ STATE navigating() {
 }
 
 object_state object_detected() {
-    float left_ir = get_left_IR();
+    float left_ir = get_front_left_IR();
     float ultrasonic = TriggerUltrasonic();
-    float right_ir = get_right_IR();
+    float right_ir = get_front_right_IR();
     float distance_in_front = min(left_ir, min(ultrasonic, right_ir));
     SerialCom->println("L_IR: " + String(left_ir) + " | Ultrasonic: " + String(ultrasonic) + " | R_IR: " + String(right_ir) + " cm");
-    float obstacle_threshold = 15.0f;  // in cm
+    float obstacle_threshold = 15.0f;
     uint16_t PT_readings[4];
 
     if (distance_in_front > obstacle_threshold) {
@@ -35,19 +35,23 @@ object_state object_detected() {
         }
         SerialCom->println(String(PT_readings[0]) + " | " + String(PT_readings[1]) + " | " + String(PT_readings[2]) + " | " + String(PT_readings[3]));
 
-    int pt_min = PT_readings[0];
-    int pt_sum = 0;
-    for (uint8_t i = 0; i < 4; i++) {
-      pt_sum += PT_readings[i];
-      if (PT_readings[i] < pt_min) pt_min = PT_readings[i];
-    }
+        uint32_t pt_sum = 0;
+        uint16_t pt_max = 0;
+        for (uint8_t i = 0; i < 4; i++) {
+            pt_sum += PT_readings[i];
+            if (PT_readings[i] > pt_max) pt_max = PT_readings[i];
+        }
 
-    if (pt_min < 80 && pt_sum < 2200) {
-      return OBSTACLE;
-    } else {
-      return FIRE;
+        if (pt_sum < 50) {
+            return OBSTACLE;
+        } else if (PT_readings[0] == pt_max || PT_readings[3] == pt_max) {
+            return OBSTACLE;
+        } else if (PT_readings[0] + PT_readings[3] < 25) {
+            return OBSTACLE;
+        } else {
+            return FIRE;
+        }
     }
-  }
 }
 
 // drive forward with fire tracking
